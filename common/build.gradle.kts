52aa8ba89c96007112b0254e02242709e82bf8d5
IMS212 <ims@irisshaders.dev>
2024-12-02 15:08:44 -0800

    1.8.1

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
 
     modCompileOnly("net.fabricmc.fabric-api:fabric-renderer-api-v1:3.2.9+1172e897d7")
 
-    modImplementation("maven.modrinth", "sodium", "mc1.21.1-0.6.0-fabric")
+    modImplementation("maven.modrinth", "sodium", "mc1.21.1-0.6.1-fabric")
     modCompileOnly("org.antlr:antlr4-runtime:4.13.1")
     modCompileOnly("io.github.douira:glsl-transformer:2.0.1")
     modCompileOnly("org.anarres:jcpp:1.4.14")
